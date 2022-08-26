1793fa4de9894ef514a01176523865a4ec8786d9
DaniyalSarwari <iamdaniyalsarwari@gmail.com>
2022-08-26 09:13:47 +0500

    Complete task 1 & task 2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # js-assignments-by-zeeshan-hanif
 Solved JavaScript assignments given by zeeshan hanif
+
+**NOTE: You may find some outdated techniques or unconventional naming stuff which is a part of doing this assignmnets.**  
---
+++ assignment1_piaic106846/task2.js
@@ -0,0 +1,7 @@
+let studentName = prompt("Enter student name: ");
+let studentAge = prompt("Enter student age: ");
+let studentCertification = prompt("Enter student certification: ");
+
+alert(studentName);
+alert(studentAge);
+alert(studentCertification);
