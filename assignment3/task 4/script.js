468c4623bdd85d53478ea902cb8fcd49514d7811
DaniyalSarwari <iamdaniyalsarwari@gmail.com>
2022-09-05 20:43:10 +0500

    task 4 completed

---
+++ assignment3/task 4/script.js
@@ -0,0 +1,5 @@
+let citiesList = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+
+document.getElementById("list").innerText = citiesList;
+
+document.getElementById("selected-list").innerText = citiesList.slice( 2,4 );
