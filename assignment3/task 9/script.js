1264840c905437ababa0e15cbb24700e9a024666
DaniyalSarwari <iamdaniyalsarwari@gmail.com>
2022-09-06 23:40:58 +0500

    task 9 completed

---
+++ assignment3/task 9/script.js
@@ -0,0 +1,13 @@
+// sort an array from smallest to largest
+
+let num_list = [ 24, 53, 78, 91, 12 ];
+document.getElementById("ini-array").innerText = num_list;
+
+let largest_number = num_list[0];
+for( let i = 0; i < num_list.length; i++ ){
+    for( let j = i + 1; j < num_list.length; j++ ){
+        largest_number = ( num_list[j] > largest_number ) ? num_list[j] : largest_number;
+    }
+}
+
+document.getElementById("largest-num").innerText = largest_number;
